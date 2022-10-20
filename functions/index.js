cee65bda6e3969163dd5aac0e22836935676a4b5
Todd Albert <todd@bocacode.com>
2022-10-20 15:51:54 -0400

    update furniture

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
 import functions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
-import { getAllFurniture } from './src/furniture.js'
+import { getAllFurniture, addNewFurniture, updateFurniture } from './src/furniture.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/furniture', getAllFurniture)
+app.post('/furniture', addNewFurniture)
+app.patch('/furniture/:furnitureId', updateFurniture)
 
 export const api = functions.https.onRequest(app)
--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import dbConnect from "./dbConnect.js";
 
 export async function getAllFurniture(req, res) {
@@ -12,4 +13,33 @@ export async function getAllFurniture(req, res) {
     });
   // send back the array of furniture
   res.send(collection);
+}
+
+export async function addNewFurniture(req, res) {
+  // get new furniture from the body of the request
+  const { brand, model, type, price } = req.body
+  const newFurniture = { brand, model, type, price: Number(price) }
+  // connect to database
+  const db = dbConnect()
+  // put this new furniture into our furniture collection in our db
+  await db.collection('furniture').insertOne(newFurniture)
+  // catch errors and send with status 500
+    .catch(err => {
+      res.status(500).send(err)
+      return
+    })
+  // return response with 201 all bueno
+  res.status(201).send({ message: 'Furniture added' })
+}
+
+export async function updateFurniture(req, res) {
+  const { furnitureId } = req.params
+  const db = dbConnect()
+  await db.collection('furniture')
+    .findOneAndUpdate({ _id: new ObjectId(furnitureId) }, { $set: req.body })
+    .catch(err => {
+      res.status(500).send(err)
+      return
+    })
+  res.status(202).send({ message: 'Updated furniture '})
 }
